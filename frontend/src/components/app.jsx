8631e9005617a8f7833c4ac97bbf381534f46564
rizzacarla <rizzacarla.12@gmail.com>
2021-04-09 19:02:50 -0700

    Add navbar

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import MainPageContainer from './main/main_page_container';
+import NavBarContainer from './navbar/navbar_container';
 
 const App = () => (
-  <Switch>
-    <Route exact path='/main' component={MainPageContainer}></Route>
-    <Redirect to='/main' />
-  </Switch>
+  <div>
+    <NavBarContainer/>
+    <Switch>
+      <Route exact path='/main' component={MainPageContainer}></Route>
+      <Redirect to='/main' />
+    </Switch>
+  </div>
 );
 
 export default App;
---
+++ frontend/src/components/navbar/navbar_container.js
@@ -0,0 +1,12 @@
+import {connect} from 'react-redux';
+import Navbar from './navbar';
+
+const msp = state => ({
+
+})
+
+const mdp = dispatch => ({
+
+})
+
+export default connect(msp, mdp)(Navbar);
