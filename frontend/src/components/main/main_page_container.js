a6340c0b549476f4455c245b49f4295cf5ef1839
rizzacarla <rizzacarla.12@gmail.com>
2021-04-12 12:16:20 -0700

    Create Main page

--- frontend/src/components/main/main_page_container.js
+++ frontend/src/components/main/main_page_container.js
@@ -1,12 +1,15 @@
 import { connect } from 'react-redux';
 import MainPage from './main_page';
+import { fetchStudent, fetchStudents } from '../../actions/student_actions'
 
 const msp = state => ({
-
+  student: state.studentReducer,
+  students: state.studentReducer,
 })
 
 const mdp = dispatch => ({
-
+  fetchStudent: () => dispatch(fetchStudent()),
+  fetchStudents: () => dispatch(fetchStudents()),
 })
 
 export default connect(msp, mdp)(MainPage);
