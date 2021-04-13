058cf134359f1d0fb52aa2374bc819be7f247651
rizzacarla <rizzacarla.12@gmail.com>
2021-04-13 06:24:36 -0700

    Add sorting dropdown to navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './navbar.css';
 
 class Navbar extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchStudents()
+  }
+
+  AZ (students) {
+    this.props.setCurrentStudents(Object.values(students).sort((a, b) => (a.name > b.name) ? 1 : -1))
+  }
+
+  ZA () {
+
+  }
+
   render() {
+    if (!this.props.student || !this.props.students) {
+      return null
+    }
+
     return (
       <div className="navbar-container">
-        <h1>Student Records</h1>
+        <section className="navbar-left">
+          <h1>Student Records</h1>
+        </section>
+        <section className="navbar-right">
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Sort
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Header>Sort Students By:</Dropdown.Header>
+              <Dropdown.Item onSelect={() => this.AZ(this.props.students)}>A to Z</Dropdown.Item>
+              <Dropdown.Item onSelect={this.ZA}>Z to A</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </section>
       </div>
     )
   }
--- frontend/src/components/navbar/navbar_container.js
+++ frontend/src/components/navbar/navbar_container.js
@@ -1,12 +1,17 @@
 import {connect} from 'react-redux';
 import Navbar from './navbar';
+import { fetchStudent, fetchStudents } from '../../actions/student_actions';
+import { setCurrentStudents } from '../../actions/sorting_students';
 
 const msp = state => ({
-
+  student: state.studentReducer,
+  students: state.studentReducer,
 })
 
 const mdp = dispatch => ({
-
+  fetchStudent: () => dispatch(fetchStudent()),
+  fetchStudents: () => dispatch(fetchStudents()),
+  setCurrentStudents: (currentStudents) => dispatch(setCurrentStudents(currentStudents)),
 })
 
 export default connect(msp, mdp)(Navbar);
