eb57205c6f8a9e35c0503c4ce8a883a8997b4f1c
rizzacarla <rizzacarla.12@gmail.com>
2021-04-13 09:13:53 -0700

    Add search and sort by dropdown

--- frontend/src/components/navbar/navbar_container.js
+++ frontend/src/components/navbar/navbar_container.js
@@ -2,16 +2,19 @@ import {connect} from 'react-redux';
 import Navbar from './navbar';
 import { fetchStudent, fetchStudents } from '../../actions/student_actions';
 import { setCurrentStudents } from '../../actions/sorting_students';
+import { setSearchTerm } from '../../actions/search_actions';
 
 const msp = state => ({
   student: state.studentReducer,
   students: state.studentReducer,
+  searchTerm: state.searchReducer,
 })
 
 const mdp = dispatch => ({
   fetchStudent: () => dispatch(fetchStudent()),
   fetchStudents: () => dispatch(fetchStudents()),
   setCurrentStudents: (currentStudents) => dispatch(setCurrentStudents(currentStudents)),
+  setSearchTerm: (searchTerm) => dispatch(setSearchTerm(searchTerm)),
 })
 
 export default connect(msp, mdp)(Navbar);
