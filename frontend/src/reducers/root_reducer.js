88b04190dc87524754425722952bf7aeea2e872f
rizzacarla <rizzacarla.12@gmail.com>
2021-04-13 06:24:54 -0700

    Add sorting reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import studentReducer from './student_reducer';
+import sortingReducer from './sorting_reducer';
 
 const RootReducer = combineReducers({
   studentReducer,
+  sortingReducer,
 })
 
 export default RootReducer;
